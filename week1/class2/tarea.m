0432f573b6e3fb68613b9254f94cfe3005a6b9fd
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-07-27 17:11:20 -0600

    Week1: Update

--- week1/class2/tarea.m
+++ week1/class2/tarea.m
@@ -1,19 +1,22 @@
-function [current_tol] = compute_tol(m):
-    current_tol = 2**(2 * (m + 1)) / (factorial(m)*(2 * (m + 1) + 1));
-end
+close all;
+clear;
 
-function [aprox, m] = ejemplo_integral(tol):
+function current_tol = compute_tol (m)
+    current_tol = 2**(2 * (m + 1)) / (factorial(m)*(2 * (m + 1) + 1));
+endfunction
+
+function [aprox, m] = ejemplo_integral (tol)
     aprox = 0;
     m = -1;
     current_tol = 1;
     
     while (current_tol > tol)
         m = m + 1;
-        aprox = aprox (-1)**m * 2**(2 * m) / (factorial(m) * (2 * m + 1));
-        current_tol = compute_tol(m);
-    end
-end
+        aprox = aprox + (-1)**m * 2**(2 * m) / (factorial(m) * (2 * m + 1));
+        current_tol = compute_tol (m);
+    endwhile
+endfunction
 
 % Main
-[aprox, m] = ejemplo_integral(0.00000000000001);
+[aprox, m] = ejemplo_integral (0.000001);
 printf('Aprox = %f, m = %i', aprox, m);
