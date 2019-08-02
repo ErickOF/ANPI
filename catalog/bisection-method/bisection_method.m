beb520c06f4c352e0ac7c3c7ba40948c28c9e0ea
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-08-01 19:33:35 -0600

    Catalog. Bisection method: MATLAB code

--- catalog/bisection-method/bisection_method.m
+++ catalog/bisection-method/bisection_method.m
@@ -4,25 +4,25 @@ close all;
 
 % Bisection method
 function [xAprox, iter] = bisection (a, b, tol, f)
-	if (f(a) * f(b) < 0)
-		xAprox = (a + b) / 2;
-		iter = 0;
-		
-		while (abs(f(xAprox)) > tol)
-			if (f(a) * f(xAprox) < 0)
-				b = xAprox;
-			else
-				a = xAprox;
-			endif
-			
-			xAprox = (a + b) / 2;
-			iter = iter + 1;
-		endwhile
-	else
-		error("Condiciones no garantizan el cero de la función");
-	endif
+  if (f(a) * f(b) < 0)
+    xAprox = (a + b) / 2;
+    iter = 0;
+        
+    while (abs(f(xAprox)) > tol)
+      if (f(a) * f(xAprox) < 0)
+        b = xAprox;
+      else
+        a = xAprox;
+      endif
 
-	return;
+      xAprox = (a + b) / 2;
+      iter = iter + 1;
+    endwhile
+  else
+    error("Condiciones no garantizan el cero de la funciï¿½n");
+  endif
+
+  return;
 endfunction
 
 
