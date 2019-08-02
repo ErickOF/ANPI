a4e1e1ec6a1cf246e34c4a4796d85fe7d78cc301
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-08-02 14:10:09 -0600

    Catalog. Newton Raphson method: MATLAB code

---
+++ catalog/newton-raphson-method/newton_raphson_method.m
@@ -0,0 +1,40 @@
+clear;
+close all;
+
+pkg load symbolic;
+
+
+% Newton Raphson method
+function [xAprox, iter] = newton_raphson (func, x0, tol)
+  xAprox(1) = x0;
+  iter = 0;
+
+  syms f(x);
+  f(x) = func;
+  
+  while (abs(func(xAprox(end))) > tol)
+    xk = xAprox(end);
+    df = diff(f);
+    xAprox(end + 1) = xk - func(xk)/double(df(xk));
+    iter = iter + 1;
+  endwhile
+
+  return;
+endfunction
+
+
+% Main
+x0 = 3/4;
+tol = 0.0000000001;
+func = @(x) cos(2*x).^2-x.^2;
+[xAprox, iter] = newton_raphson (func, x0, tol);
+printf('xAprox = [');
+printf(' %f ', xAprox);
+printf(']\nIteraciones = %i', iter);
+
+% Plot
+x = 0:1:iter;
+y = abs(func(xAprox));
+plot(x, y);
+xlabel('iteraciones');
+ylabel('|f(xk)|');
