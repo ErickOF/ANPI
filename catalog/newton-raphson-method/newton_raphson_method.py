81735acb0fda1c65003438c65548156707684296
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-08-02 13:57:03 -0600

    Catalog. Newton Raphson method: Python code

---
+++ catalog/newton-raphson-method/newton_raphson_method.py
@@ -0,0 +1,31 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import misc
+
+
+# Newton Raphson method
+def newton_raphson(f, x0, tol):
+    xAprox = np.array([x0])
+    _iter = 0
+
+    while (abs(f(xAprox[-1])) > tol):
+        xk = xAprox[-1]
+        f_derivative = misc.derivative(f, xk, dx=1e-6)
+        xAprox = np.append(xAprox, xk - f(xk)/f_derivative)
+        _iter += 1
+
+    return xAprox, _iter
+
+def plot_function(x, y):
+    plt.plot(x, y)
+    plt.show()
+
+
+if __name__ == '__main__':
+    x0 = 3/4
+    tol = 0.0000000001
+    func = lambda x: (np.cos(2*x))**2 - x**2
+    xAprox, _iter = newton_raphson(func, x0, tol)
+    print('xAprox = {}\nIteraciones = {}'.format(xAprox[-1], _iter))
+    plot_function(np.linspace(0, _iter, _iter+1), np.abs(func(xAprox)))
