e150d9c1ce09eea78596da63bcacbde0980c7dc0
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-08-06 10:38:50 -0600

    Catalog. Newton Raphson method: Python code

--- catalog/newton-raphson-method/newton_raphson_method.py
+++ catalog/newton-raphson-method/newton_raphson_method.py
@@ -5,14 +5,14 @@ from scipy import misc
 
 
 # Newton Raphson method
-def newton_raphson(f, x0, tol):
+def newtonRaphson(f, x0, tol):
     xAprox = np.array([x0])
     _iter = 0
 
     while (abs(f(xAprox[-1])) > tol):
         xk = xAprox[-1]
         df = misc.derivative(f, xk, dx=1e-6)
-        xAprox = np.append(xAprox, xk - f(xk)/df)
+        xAprox = np.append(xAprox, xk - f(xk) / df)
         _iter += 1
 
     return xAprox, _iter
@@ -23,9 +23,9 @@ def plot_function(x, y):
 
 
 if __name__ == '__main__':
-    x0 = 3/4
+    x0 = 3 / 4
     tol = 0.0000000001
-    func = lambda x: (np.cos(2*x))**2 - x**2
-    xAprox, _iter = newton_raphson(func, x0, tol)
+    func = lambda x: (np.cos(2 * x))**2 - x**2
+    xAprox, _iter = newtonRaphson(func, x0, tol)
     print('xAprox = {}\nIteraciones = {}'.format(xAprox[-1], _iter))
-    plot_function(np.linspace(0, _iter, _iter+1), np.abs(func(xAprox)))
+    plot_function(np.linspace(0, _iter, _iter + 1), np.abs(func(xAprox)))
