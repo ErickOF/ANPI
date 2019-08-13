846dca0c71c85111382cc756627baacd84e55550
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2019-08-13 16:59:15 -0600

    Catalog. Secant method: Python code

--- catalog/1.3.secant-method/secant_method.py
+++ catalog/1.3.secant-method/secant_method.py
@@ -2,23 +2,22 @@ import math
 
 
 # Secant method
-def secante(f, xk_1, xk, tol):
+def secant(xk_1, xk, tol, f):
     _iter = 1
 
     while (abs(xk - xk_1) / abs(xk)) > tol:
         x_temp = xk - (xk - xk_1) / (f(xk) - f(xk_1)) * f(xk)
-		xk_1 = xk
-		xk = x_temp
-		_iter += 1
+        xk_1 = xk
+        xk = x_temp
+        _iter += 1
 
     return xk, _iter
 
 # Prueba
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     x0 = 0
     x1 = 1
-    tol = 0.1
-    func = lambda x: math.e**x - x - 2
-    xk, _iter = secant (a, b, tol, func)
-    print('xAprox = {}\nIteraciones = {}'.format(xAprox, _iter))
+    tol = 0.01
+    func = lambda x: math.e**(-x**2) - x
+    xk, _iter = secant(x0, x1, tol, func)
+    print('xk = {}\nIteraciones = {}'.format(xk, _iter))
